f5a77fe61c3f86aa2c7f61afba3d70b7f459f1ea
valentinptr <valentin.ptr@icloud.com>
2023-10-25 17:45:46 +0200

    Ajout des murs

--- game.py
+++ game.py
@@ -1,4 +1,13 @@
 from objects import *
+import pygame
+
+BACKGROUND_COLOR = (50, 50, 50)
+BACKGROUND_WALL = 'black'
+WALL_THICKNESS = 15
+WALL_LENGTH = 600
+WALL_START_POSITION_X = 50
+WALL_END_POSITION_X = 550
+WALL_START_POSITION_Y = 100
 class Engine():
     def __init__(self):
         self.fruits = []
@@ -6,9 +15,25 @@ class Engine():
 
     def update(self, dt):
         for fruit in self.fruits:
-            fruit.y += 0.1
+            fruit.y += 1
     def drawBackground(self, screen):
-        screen.fill((50, 50, 50))
+        screen.fill(BACKGROUND_COLOR)
+        #Mur gauche
+        pygame.draw.line(screen, BACKGROUND_WALL,
+                         (WALL_START_POSITION_X, WALL_START_POSITION_Y),
+                         (WALL_START_POSITION_X, WALL_START_POSITION_X + WALL_LENGTH),
+                         WALL_THICKNESS )
+        # Mur droit
+        pygame.draw.line(screen, BACKGROUND_WALL,
+                         (WALL_END_POSITION_X, WALL_START_POSITION_Y),
+                         (WALL_END_POSITION_X, WALL_START_POSITION_X + WALL_LENGTH),
+                         WALL_THICKNESS)
+
+        # Mur bas
+        pygame.draw.line(screen, BACKGROUND_WALL,
+                         (WALL_START_POSITION_X - (WALL_THICKNESS / 2), WALL_START_POSITION_X + WALL_LENGTH + (WALL_THICKNESS / 2)),
+                         (WALL_END_POSITION_X + (WALL_THICKNESS / 2), WALL_START_POSITION_X + WALL_LENGTH + (WALL_THICKNESS / 2)),
+                         WALL_THICKNESS)
 
     def drawFruits(self, screen):
         for fruit in self.fruits:
